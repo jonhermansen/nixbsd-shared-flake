95f08e4cd2e040960228f6b1eb304f1e991b3294
Jon Hermansen <jon@jh86.org>
2025-12-11 11:13:17 -0500

    it does something but it's not quite right

--- configuration.nix
+++ configuration.nix
@@ -2,29 +2,41 @@
   config,
   lib,
   pkgs,
+  nixbsdBootloader ? null,
   ...
 }:
 
+let
+  # Create a script that copies NixBSD bootloader to ESP
+  installNixBSD = lib.optionalString (nixbsdBootloader != null) ''
+    # Copy NixBSD bootloader to ESP
+    echo "Installing NixBSD bootloader..."
+    mkdir -p /boot/efi/nixbsd
+    cp -Lrf ${nixbsdBootloader}/* /boot/efi/nixbsd/
+    echo "NixBSD bootloader installed to /boot/efi/nixbsd/"
+  '';
+in
 {
   imports = [
     ./hardware-configuration.nix
   ];
 
-  #boot.loader.systemd-boot.configurationLimit = 5;
-  #boot.loader.systemd-boot.edk2-uefi-shell.enable = true;
-  #boot.loader.systemd-boot.enable = true;
-  #boot.loader.systemd-boot.extraEntries."freebsd.conf" = ''
-  #  title FreeBSD
-  #  efi /EFI/BOOT/BOOTX64.EFI.BAK
-  #  sort-key z_freebsd
-  #'';
-  #boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.devices = [ "nodev" ];
   boot.loader.grub.enable = true;
   boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.grub.efiSupport = true;
+  
+  # Add activation script to copy NixBSD bootloader
+  system.activationScripts.installNixBSD = lib.mkIf (nixbsdBootloader != null) {
+    text = installNixBSD;
+    deps = [];
+  };
+  
   boot.loader.grub.extraEntries = ''
-    menuentry "FreeBSD" {
+    menuentry "NixBSD (FreeBSD)" {
+      chainloader /efi/nixbsd/efi/boot/bootx64.efi
+    }
+    menuentry "FreeBSD (existing)" {
       chainloader /efi/boot/bootx64.efi.bak
     }
   '';
@@ -32,25 +44,21 @@
   environment.etc."sway/config.d/zzz-custom.conf".text = ''
     set $mod Mod1
   '';
+  
   environment.systemPackages = with pkgs; [
     efibootmgr
     emacs
     foot
     git
     kitty
-    #lutris
     mesa-demos
     nix-output-monitor
     nixfmt-rfc-style
     open-vm-tools
     pciutils
     usbutils
-    #wineWowPackages.waylandFull
-    #wine64
-    #wineWowPackages.stable
-    #winetricks
   ];
-  #environment.variables.WLR_NO_HARDWARE_CURSORS = "1";
+  
   hardware.graphics.enable = true;
   hardware.nvidia = {
     modesetting.enable = true;
@@ -60,39 +68,38 @@
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
+  
   networking.hostId = "12345678";
   networking.networkmanager.enable = true;
+  
   nix.settings.experimental-features = [
     "flakes"
     "nix-command"
   ];
+  
   nixpkgs.config.allowUnfree = true;
+  
   programs.firefox.enable = true;
   programs.firefox.package = pkgs.librewolf;
   programs.fish.enable = true;
   programs.sway.enable = true;
   programs.sway.xwayland.enable = true;
   programs.virt-manager.enable = true;
+  
   security.sudo.wheelNeedsPassword = false;
-  #services.desktopManager.gnome.enable = true;
-  #services.desktopManager.plasma6.enable = true;
-  #services.displayManager.gdm.enable = true;
-  #services.displayManager.sddm.enable = true;
+  
   services.getty.autologinUser = "user";
-  #services.greetd.enable = true;
-  #services.greetd.package = pkgs.tuigreet;
-  #services.nfs.server.enable = true;
-  #services.nfs.server.exports = ''
-  #'';
   services.sshd.enable = true;
+  
   services.xserver.desktopManager.mate.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
-  #services.xserver.displayManager.lightdm.enable = true;
   services.xserver.displayManager.startx.enable = true;
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "vmware" ];
   services.xserver.windowManager.i3.enable = true;
+  
   time.timeZone = "US/Eastern";
+  
   users.users.user = {
     isNormalUser = true;
     extraGroups = [
@@ -102,6 +109,7 @@
     password = "password";
     shell = pkgs.fish;
   };
+  
   virtualisation.vmware.guest.enable = true;
   virtualisation.libvirtd = {
     enable = true;
--- flake.nix
+++ flake.nix
@@ -1,11 +1,27 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixbsd.url = "github:jonhermansen/nixbsd/main";
   };
 
-  outputs = { nixpkgs, ... }: {
+  outputs = { nixpkgs, nixbsd, ... }: 
+  let
+    system = "x86_64-linux";
+    
+    # Build a standalone derivation with just the bootloader files
+    # This avoids infinite recursion by not referencing the full config
+    nixbsdBootloader = nixbsd.packages.${system}.base.config.boot.loader.espContents;
+  in
+  {
+    # Expose the bootloader as a separate package for easy building
+    packages.${system}.nixbsd-bootloader = nixbsdBootloader;
+    
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
+      specialArgs = {
+        # Pass the bootloader derivation path
+        nixbsdBootloader = nixbsdBootloader;
+      };
       modules = [
         ./configuration.nix
       ];
